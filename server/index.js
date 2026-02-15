2b37bc028752723e402926f0816d194e473bcff0
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-02-15 16:50:19 +0530

    Signup api created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './db.js';
+import User from './models/User.js';
 
 dotenv.config();
 
@@ -17,9 +18,38 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
   res.send('Welcome to Tiny Tours API!');
 });
+
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
+
+app.post('/signup', async (req, res) => {
+  const { username, email, password, mobile, country,city } = req.body;
+  const newUser = new User({
+    username,
+    email,
+    password,
+    mobile,
+    country,
+    city
+  });
+  try{
+    const savedUser = await newUser.save();
+    return res.json({
+      success: true,
+      message: 'User registered successfully',
+      data: savedUser
+    });
+  }
+  catch(error){
+    return res.json({
+      success: false,
+      message: `Error registering user : ${error.message}`,
+      data: null
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
