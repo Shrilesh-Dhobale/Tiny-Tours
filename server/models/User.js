2243179a1655b54eaafdc20beba0d6758d150375
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-02-15 16:13:42 +0530

    Schema created

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,26 @@
-import mongoose from 'mongoose';
+import {Schema,model} from 'mongoose';
 
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobile: {
+        type: String,
+        },
+    password: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+    },
+    city: {
+        type: String,
+    }
+});
