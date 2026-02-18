0458e137fa321cd410972a4e55bcdf76c22480fb
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-02-18 14:47:48 +0530

    Schema design for user

--- server/models/Tour.js
+++ server/models/Tour.js
@@ -1,1 +1,34 @@
 import {Schema, model} from 'mongoose';
+
+const tourSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },  
+    description: {
+        type: String,
+        required: true,
+    },
+    cities: {
+        type: [String],
+        default: [],
+    },
+    duration: {
+        type: Number,
+    },
+    photos: {
+        type: [
+            {
+                imgUrl: String,
+                title: String,
+                description: String,
+            },
+        ],
+        default: [],
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+});
